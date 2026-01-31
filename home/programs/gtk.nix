cd584ea2f7d586f9fe26db979f15cff0706df5d4
asteriau <dorahaladita@gmail.com>
2026-01-31 17:45:44 +0200

    boom

--- home/programs/gtk.nix
+++ home/programs/gtk.nix
@@ -8,7 +8,7 @@
   home.pointerCursor = {
     package = pkgs.bibata-cursors;
     name = "Bibata-Modern-Classic";
-    size = 16;
+    size = 32;
     gtk.enable = true;
     x11.enable = true;
   };
@@ -19,7 +19,7 @@
     font = {
       name = "Inter";
       package = pkgs.google-fonts.override { fonts = [ "Inter" ]; };
-      size = 9;
+      size = 11;
     };
 
     gtk2.configLocation = "${config.xdg.configHome}/gtk-2.0/gtkrc";
