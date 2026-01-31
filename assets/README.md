830650802ae1ed6e70720e37f1422d3e8095ac84
asteriau <dorahaladita@gmail.com>
2026-01-31 09:46:22 +0200

    fix wallpaper hash

---
+++ assets/README.md
@@ -0,0 +1,2 @@
+all the static assets used in the repo or in my flake will go here
+if i do it otherwise hash mismatches become a problem because links tend to change
--- home/specialisations.nix
+++ home/specialisations.nix
@@ -2,9 +2,9 @@
   theme = {
     wallpaper =
       let
-        url = "https://github.com/asteriau/dotfiles/blob/main/home/wallpapers/pink.jpg?raw=true";
-        sha256 = "243e1c1e92894bc80879ef803dc169e78fa8e887703d95ce53a4474dfd61918f";
-        ext = "jpg";
+        url = "https://github.com/asteriau/dotfiles/blob/main/assets/wallpapers/furina.jpeg?raw=true";
+        sha256 = "d3a09f48f86a8de83654ad7828514133600f837e5522aa5545946fe8c73ebe23";
+        ext = "jpeg";
       in
       builtins.fetchurl {
         name = "wallpaper-${sha256}.${ext}";
