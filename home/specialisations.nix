46b001a8cae3a47a5a4075e1acc1bdd5174dc987
asteriau <dorahaladita@gmail.com>
2026-02-14 16:37:00 +0200

    update wallpaper

--- home/specialisations.nix
+++ home/specialisations.nix
@@ -2,9 +2,9 @@
   theme = {
     wallpaper =
       let
-        url = "https://github.com/asteriau/dotfiles/blob/main/assets/wallpapers/cat.png?raw=true";
-        sha256 = "c1989f287d8a63f05a644cebd24e3f58db736a623704ee1d61915befe85a4f13";
-        ext = "cat";
+        url = "https://github.com/asteriau/dotfiles/blob/main/assets/wallpapers/angel.jpg?raw=true";
+        sha256 = "a9be082f89c3039a4c99d7470658d600b1f02d640d5ba7725532ddb68368065c";
+        ext = "jpg";
       in
       builtins.fetchurl {
         name = "wallpaper-${sha256}.${ext}";
