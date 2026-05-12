8793761e47a5d310ece43a6114d650b088e8504c
asteria <dorahaladita@gmail.com>
2026-05-12 12:53:57 +0300

    hosts/meow: introduce profile.nix

---
+++ hosts/meow/profile.nix
@@ -0,0 +1,9 @@
+{
+  user = "laura";
+  name = "asteria";
+  email = "dorahaladita@gmail.com";
+  hostName = "meow";
+  flakePath = "/home/laura/Documents/code/dotfiles";
+  sshSigningKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICcr1LDS9jd0eKlI+2T7exP29Q01lMtXioB5PQQsio1V laura@meow";
+  timeZone = "Europe/Bucharest";
+}
