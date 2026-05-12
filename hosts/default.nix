55ae3f717f5a75c837f5f07e0562d12d27fec51b
asteria <dorahaladita@gmail.com>
2026-05-12 12:56:11 +0300

    hosts: thread profile through specialArgs

--- hosts/default.nix
+++ hosts/default.nix
@@ -6,24 +6,37 @@
 {
   flake.nixosConfigurations =
     let
-      # shorten paths
       inherit (inputs.nixpkgs.lib) nixosSystem;
 
       homeImports = import "${self}/home/profiles";
 
       mod = "${self}/system";
-      # get the basic config to build on top of
       inherit (import mod) laptop;
 
-      # get these into the module system
-      specialArgs = { inherit inputs self; };
+      mkHost =
+        host: extraModules:
+        let
+          profile = import ./${host}/profile.nix;
+          specialArgs = { inherit inputs self profile; };
+        in
+        nixosSystem {
+          inherit specialArgs;
+          modules = extraModules ++ [
+            ./${host}
+            {
+              home-manager = {
+                users.${profile.user}.imports = homeImports."${profile.user}@${host}" or homeImports.server;
+                extraSpecialArgs = specialArgs;
+                backupFileExtension = ".hm-backup";
+              };
+            }
+          ];
+        };
     in
     {
-      meow = nixosSystem {
-        inherit specialArgs;
-        modules = laptop ++ [
-          ./meow
-
+      meow = mkHost "meow" (
+        laptop
+        ++ [
           "${mod}/programs/gamemode.nix"
           "${mod}/programs/hyprland"
           "${mod}/programs/games.nix"
@@ -31,33 +44,14 @@
           "${mod}/network/spotify.nix"
 
           "${mod}/services/gnome-services.nix"
+        ]
+      );
 
-          {
-            home-manager = {
-              users.laura.imports = homeImports."laura@meow";
-              extraSpecialArgs = specialArgs;
-              backupFileExtension = ".hm-backup";
-            };
-          }
-        ];
-      };
-
-      nixos = nixosSystem {
-        inherit specialArgs;
-        modules = [
-          ./wsl
-          "${mod}/core/users.nix"
-          "${mod}/nix"
-          "${mod}/programs/zsh.nix"
-          "${mod}/programs/home-manager.nix"
-          {
-            home-manager = {
-              users.laura.imports = homeImports.server;
-              extraSpecialArgs = specialArgs;
-              backupFileExtension = ".hm-backup";
-            };
-          }
-        ];
-      };
+      nixos = mkHost "wsl" [
+        "${mod}/core/users.nix"
+        "${mod}/nix"
+        "${mod}/programs/zsh.nix"
+        "${mod}/programs/home-manager.nix"
+      ];
     };
 }
